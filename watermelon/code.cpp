5f81f3602315555421dfbc04a6383414a70782f1
GersenX3 <galdl1438@gmail.com>
2023-02-02 11:23:14 -0600

    Uptaded my code whit the first problem and the book

---
+++ watermelon/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int watermelon;
+    cin>>watermelon;
+    if(watermelon%2==0 && watermelon>2){
+        cout<<"YES";
+    }
+    else{cout<<"NO";}
+    
+    return 0;
+}
