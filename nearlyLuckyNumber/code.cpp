444b79513d0de5e8c7682ebd8c3eb408b7e8aca5
Angelemk <angeljesuslopezduran@gmail.com>
2023-02-22 22:57:13 -0600

    i didnt solved the problem

---
+++ nearlyLuckyNumber/code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string answer="YES";
+    string n;
+    cin>>n;
+    cout<<n<<endl;
+    cout<<answer<<endl;
+    int popo; cin>>popo;
+    return 0;
+}
